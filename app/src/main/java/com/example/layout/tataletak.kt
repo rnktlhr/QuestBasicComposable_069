20972a34da88fb72dd12e0fa567dca52e33337c4
rnktlhr <rinakitluhur10@gmail.com>
2025-10-16 14:25:01 +0700

    menambahkan tataletak row pada tataletak.kt

--- app/src/main/java/com/example/layout/tataletak.kt
+++ app/src/main/java/com/example/layout/tataletak.kt
@@ -32,3 +32,15 @@ fun TataLetakColumn(modifier: Modifier) {
         Text(text = "Komponen4")
     }
 }
+@Composable
+fun TataLetakRow(modifier: Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
