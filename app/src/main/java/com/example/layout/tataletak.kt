812e30ebe0e6138576a253abeaf25830136618b4
rnktlhr <rinakitluhur10@gmail.com>
2025-10-16 18:33:48 +0700

    menambahkan komponen tataletak RowColumn pada tataletak.kt

--- app/src/main/java/com/example/layout/tataletak.kt
+++ app/src/main/java/com/example/layout/tataletak.kt
@@ -92,7 +92,10 @@ fun TataLetakRowColumn(modifier: Modifier) {
         modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceEvenly
     ) {
-
+        Column {
+            Text(text = "Komponen1Kolom1")
+            Text(text = "Komponen2Kolom1")
+            Text(text = "Komponen3Kolom1")
         }
     }
 }
