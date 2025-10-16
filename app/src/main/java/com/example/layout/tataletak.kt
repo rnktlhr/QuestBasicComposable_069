ec355b3adee5cafae27cb50befc90061a7165b8d
rnktlhr <rinakitluhur10@gmail.com>
2025-10-16 14:22:30 +0700

    menambahkan tataletak coloum pada tataletak.kt

--- app/src/main/java/com/example/layout/tataletak.kt
+++ app/src/main/java/com/example/layout/tataletak.kt
@@ -22,3 +22,13 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+@Composable
+fun TataLetakColumn(modifier: Modifier) {
+    Column(modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
